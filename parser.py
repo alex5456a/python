f30424a2d60b2866fc40442db8707398a37b56dc
alex5456a <vasilyevspb@gmail.com>
2025-07-09 12:02:57 +0300

    add lines reverse

--- parser.py
+++ parser.py
@@ -8,4 +8,20 @@ parser.add_argument('--limit', '-l', type=int, help='the number of line to proce
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
-print(args)
+
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileExistsError as err:
+    print(f"error: {err}")
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
+
+        if args.limit:
+            lines = lines[:limit]
+
+        for line in lines:
+            print(line.strip()[::1])
+            
