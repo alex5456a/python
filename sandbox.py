388cfb024b615830b60b03e9e890643ca8843705
alex5456a <vasilyevspb@gmail.com>
2025-08-05 16:07:42 +0300

    functions with return

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,9 @@
 def area(radius, pi=3.14):
-    print(pi*radius*radius)
-
-area(10)
+    result = pi*radius*radius
+    return result
+def cost(circle_area, cost_per_sqm):
+    total_cost = circle_area*cost_per_sqm
+    return total_cost
+calcutated_area = area(10, 3.15)
+tc = cost(calcutated_area, 2)
+print(tc)
