354dc025c1f3f6d425881ea614db5830192717c4
alex5456a <vasilyevspb@gmail.com>
2025-08-24 00:34:12 +0300

    array fancy indexing sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-names = np.array(['Shaun', 'Geraldo', 'Bob', 'Jim'])
-ages = np.array([30, 30, 40, 40])
-drinks_coffee = np.array([True, False, True, False])
+arr = np.arange(0, 21, 2)
+indices = np.array([0, 1, -2, -1])
+even_indices = np.arange(0, arr.size, 2)
+odd_indices = np.arange(1, arr.size, 2)
 
-ind = np.lexsort((names, ages))
-# for i in ind:
-#     print(f"{names[i]} + {ages[i]} + {drinks_coffee[i]}")
+# print(indices)
+# print(arr[even_indices])
+# print(arr[odd_indices])
 
-for name, age, coffee in zip(names[ind], ages[ind], drinks_coffee[ind]):
-    print(f'{name} - {age} - {coffee}')
+arr_2d = np.arange(0, 25).reshape(5, 5)
+print(arr_2d)
+print()
+print(arr_2d[[0, 2, 4], -3:])
