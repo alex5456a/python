a2b608dad03bb2eac4bdc9c6f57f16b1bb6c412c
alex5456a <vasilyevspb@gmail.com>
2025-08-23 04:13:53 +0300

    array sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,21 @@
-def remove_duplicates(numbers):
-    new_list = []
-    for number in numbers:
-        if number not in new_list:
-            new_list.append(number)
-    return new_list
+import numpy as np
 
-ids = [1, 2 , 2, 1, 3, 4, 5]
-print(remove_duplicates(ids))
+test = np.arange(4, 14, 2)
+print(test)
+
+test2 = np.array([1, 2, 3, 4])
+print(test2 + np.ones(4))
+print(test2 + test2)
+
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# arr2 = np.array([10, 20, 30, 40, 50, 60, 100])
+# print(arr * arr2)
+
+# arr[0] = 4444
+# arr[-1] = 888
+# print(arr)
+
+# last_four = arr[-4:]
+# print(last_four)
+# last_four[0] = 3534545
+# print(arr)
