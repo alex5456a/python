169ac2bb21a37dbf163e5c4ba18570d885264418
alex5456a <vasilyevspb@gmail.com>
2025-08-24 03:36:16 +0300

    array 3 execises sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-arr = np.arange(0, 21, 2)
-indices = np.array([0, 1, -2, -1])
-even_indices = np.arange(0, arr.size, 2)
-odd_indices = np.arange(1, arr.size, 2)
+numbers = np.arange(0, 25)
+[print(num) for num in numbers[[0, 1, -2, -1]] if (num > 5)]
+[print(num) for num in numbers[:(len(numbers)//2)] if (num > 5)]
 
-# print(indices)
-# print(arr[even_indices])
-# print(arr[odd_indices])
+numbers_2d = numbers.reshape(5, 5)
+print(numbers_2d)
+summary = np.sum(numbers_2d, axis=1).tolist()
+for s in summary:
+    if s > 50:
+        [print(num) for num in numbers_2d[summary.index(s),[0, -1]]]
 
-arr_2d = np.arange(0, 25).reshape(5, 5)
-print(arr_2d)
-print()
-print(arr_2d[[0, 2, 4], -3:])
+#[print(x) for x in numbers if x % 2 == 0]
+# print(numbers[numbers % 2 == 0])
