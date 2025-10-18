c6411a6249fd342014a6a9c9cc322c5d38d57747
alex5456a <vasilyevspb@gmail.com>
2025-10-18 22:30:19 +0300

    request BS title with points

--- scraping/upload_news.py
+++ scraping/upload_news.py
@@ -11,9 +11,23 @@ print(soup.title.text)
 title_rows = soup.find_all('tr', class_='athing')
 upvotes_rows = [title_row.next_sibling for title_row in title_rows]
 
-titles = []
+# titles = []
 
-for tr in title_rows:
-    title_element = tr.find('span', class_='titleline')
-    titles.append(title_element.text)
-print(titles)
+# for tr in title_rows:
+#     title_element = tr.find('span', class_='titleline')
+#     titles.append(title_element.text)
+# print(titles)
+
+titles = [tr.find('span', class_='titleline').text for tr in title_rows]
+links = [tr.find('span', class_='titleline').find('a')['href'] for tr in title_rows]
+upvotes = [ur.find('span', class_='score').text if ur.find('span', class_='score') else '0 points' for ur in upvotes_rows]
+
+articles = []
+
+for title, link, upvote in zip(titles, links, upvotes):
+    articles.append({
+        'title': title,
+        'link': link,
+        'upvote': upvote,
+    })
+print(articles)
