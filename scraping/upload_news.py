5fe41d51aa401eb87e85eeb7429d996537dd1a36
alex5456a <vasilyevspb@gmail.com>
2025-10-18 20:34:10 +0300

    request BS title parsing

--- scraping/upload_news.py
+++ scraping/upload_news.py
@@ -5,4 +5,15 @@ response = requests.get('https://news.ycombinator.com')
 html = response.text 
 
 soup = BeautifulSoup(html, 'html.parser')
-print(soup.prettify())
+#print(soup.prettify())
+print(soup.title.text)
+
+title_rows = soup.find_all('tr', class_='athing')
+upvotes_rows = [title_row.next_sibling for title_row in title_rows]
+
+titles = []
+
+for tr in title_rows:
+    title_element = tr.find('span', class_='titleline')
+    titles.append(title_element.text)
+print(titles)
